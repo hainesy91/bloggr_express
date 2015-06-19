984ce91a51dfec865e272dfadd3839fd504ce692
Christopher Haines <hainesy91@gmail.com>
2015-06-19 09:32:59 +0100

    created a model for the comments

--- models.js
+++ models.js
@@ -10,12 +10,20 @@ var BlogSchema = new mongoose.Schema({
   post: {
     type: String,
     default: ''
-  }
+  },
 });
+
+var CommentSchema = new mongoose.Schema({
+  comment:{
+    type: String,
+    default: ''
+  }
+})
 
 //Creating a mongoose Blog model to allow us to instantiate new blog documents;
 var Blog = mongoose.model('Blog', BlogSchema)
+var Comment = mongoose.model('Comment', CommentSchema)
 
 //Making blog object available to other 
 module.exports.Blog = Blog;
-
+module.exports.Comment = Comment;
